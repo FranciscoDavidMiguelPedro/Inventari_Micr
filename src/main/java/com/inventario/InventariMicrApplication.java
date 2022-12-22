2640ac80435e078529560ee75609749687d1d4a1
FranciscoDavidMiguelPedro <fransisco.david15@gmail.com>
2022-12-21 21:27:48 -0600

    Primer proyecto subido al repo

---
+++ src/main/java/com/inventario/InventariMicrApplication.java
@@ -0,0 +1,13 @@
+package com.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventariMicrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventariMicrApplication.class, args);
+	}
+
+}
